f5393c08f27189927fe7935020a61d39ba500f0c
gkursi <itsbookx@gmail.com>
2024-02-29 16:27:52 +0200

    added darkmode.js lib

--- h/js/global.js
+++ h/js/global.js
@@ -1,9 +1,9 @@
-console.log("Loading darkmode...")
-
 import "https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js";
 
 function addDarkmodeWidget() {
   new Darkmode().showWidget()
+  console.log("Loaded darkmode!")
 }
+
 
 window.addEventListener('load', addDarkmodeWidget);

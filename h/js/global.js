372307bc9f95cffaed43c68b3897adb83726e684
qweru <itsbookx@gmail.com>
2024-04-10 21:33:12 +0300

    [/home/qweru] idek

--- h/js/global.js
+++ h/js/global.js
@@ -1,9 +1,9 @@
-import "https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js";
+// import "https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js";
 
-function addDarkmodeWidget() {
-  new Darkmode().showWidget()
-  console.log("Loaded darkmode!")
-}
+// function addDarkmodeWidget() {
+//   new Darkmode().showWidget()
+//   console.log("Loaded darkmode!")
+// }
 
 
-window.addEventListener('load', addDarkmodeWidget);
+// window.addEventListener('load', addDarkmodeWidget);

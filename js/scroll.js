1c27f66ba2f631741a085c5b3136a5e3a15f1e18
qweru <itsbookx@gmail.com>
2026-06-05 20:02:09 +0300

    mrrp

--- js/scroll.js
+++ js/scroll.js
@@ -1,17 +1,33 @@
 const content = document.querySelector("#secondary");
 const hint = document.querySelector("#hint");
 const offset = document.querySelector("#offset");
-let visible = false;
+
+let visibility = false;
+let roffset = false;
 
 function updateVisibility() {
-  if (window.scrollY <= 15 || visible) {
+  if (window.scrollY <= 15 || visibility) {
     return;
   }
 
-  visible = true;
+  visibility = true;
   content.classList.add("visible");
   hint.classList.add("invisible");
+}
+
+function removeOffset() {
+  if (
+    window.scroll <= offset.getBoundingClientRect().bottom ||
+    roffset ||
+    !visibility
+  ) {
+    return;
+  }
+
+  roffset = true;
+
   offset.classList.add("invisible");
 }
 
 window.addEventListener("scroll", updateVisibility);
+window.addEventListener("scroll", removeOffset);
